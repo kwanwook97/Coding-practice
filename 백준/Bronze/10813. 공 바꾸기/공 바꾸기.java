759eed2664361fdb572cacef9aaa6ada43e43427
wook97 <151361967+kwanwook97@users.noreply.github.com>
2024-07-29 00:55:09 +0900

    [Bronze II] Title: 공 바꾸기, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -2,14 +2,12 @@ import java.util.StringTokenizer;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.BufferedWriter;
+import java.lang.StringBuilder;
 
 public class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
         // 1~n번까지의 바구니
@@ -36,9 +34,8 @@ public class Main{
         }
         
         for(int i=0; i<arr.length; i++){
-            bw.write(arr[i] + " ");
-        }
-        bw.flush();
-        bw.close();
+            sb.append(arr[i]).append(" ");
+        }       
+        System.out.println(sb);
     }
 }    
