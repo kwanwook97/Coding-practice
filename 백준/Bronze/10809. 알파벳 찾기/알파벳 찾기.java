e93d486c5db8ec66db43eba236cd395ed35605de
wook97 <151361967+kwanwook97@users.noreply.github.com>
2024-07-29 21:49:57 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.lang.StringBuilder;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));                
+        StringBuilder sb = new StringBuilder();
+        
+        // 알파벳은 총 26개
+        int[] arr = new int[26];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = -1;
+        }
+        
+        // ASCCI코드의 규칙을 이용.
+        // 알파벳 - a = 알파벳의 순번.(0부터시작한다.)
+        String s = br.readLine();
+        int len = s.length();
+        for(int i=0; i<len; i++){
+            if(arr[s.charAt(i) - 'a'] == -1){
+                arr[s.charAt(i) - 'a'] = i;
+            }    
+        }
+        
+        // forEach문을 사용하여 출력.
+        for(int result : arr){
+            sb.append(result).append(" ");    
+        }
+        System.out.println(sb);        
+    }
+}
