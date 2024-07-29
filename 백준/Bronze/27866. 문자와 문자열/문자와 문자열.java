3940c30b29e6949c1b8ce8bd8d2fcf4843719189
wook97 <151361967+kwanwook97@users.noreply.github.com>
2024-07-29 20:26:50 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        int n = sc.nextInt()-1;
+        System.out.println(str.charAt(n));
+    }
+}
